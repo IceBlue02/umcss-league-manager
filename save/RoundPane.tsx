e5c8dc4b30a220a56e31cd31129eecd2289b3773
Ewan Massey <ewanmassey1@gmail.com>
2022-01-23 15:23:23 +0000

    Add GameBox component
    Component to display an individual game, based on the data from the Game class

--- save/RoundPane.tsx
+++ save/RoundPane.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import './RoundPane.css';
+
 
 class Game {
     names: [string, string];
@@ -12,6 +15,31 @@ class Game {
         this.round = round;
         this.gameno = gameno;
         this.hasFinished = false;
+    }
+}
+
+type GameBoxProps = {
+    game: Game
+}
+
+class GameBox extends React.Component<GameBoxProps> {
+    render () {
+        return (
+            <div className="gridbox">
+                <div className="gridbox-gameno">{this.props.game.round}<br/>.<br/>{this.props.game.gameno}</div>
+                <div className="gridbox-right">
+                    <div className="gridbox-row">
+                        <span className="name">{this.props.game.names[0]}</span>
+                        <span className="score">{this.props.game.scores[0] == null ? "" : this.props.game.scores[0]}</span>
+                    </div>
+                    <div className="gridbox-row">
+                        <span className="name">{this.props.game.names[1]}</span>
+                        <span className="score">{this.props.game.scores[1] == null ? "" : this.props.game.scores[1]}</span>
+                    </div>
+                </div>
+                
+            </div>
+        )
     }
 }
 
