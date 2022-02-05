4b6ca6f9da557239850377b7bf728df9fab4d4f9
Ewan Massey <ewanmassey1@gmail.com>
2022-02-05 23:37:41 +0000

    Add component for bye player

---
+++ src/components/ByeBox.tsx
@@ -0,0 +1,13 @@
+import {Player} from "../logic/Player"
+
+function ByeBox(props: {player: Player}) {
+    return (
+    <div className="byebox gridbox">
+        <div className="byebox-inside">
+            <span className="byebox-label">Bye: </span>
+            <span className="byebox-player"> {props.player.name}</span>
+        </div>
+    </div>)
+}
+
+export default ByeBox;
