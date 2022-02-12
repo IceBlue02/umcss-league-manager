43050d46899b38d4c36d69b69aec0eadbbf863bd
Ewan Massey <ewanmassey1@gmail.com>
2022-02-12 16:00:24 +0000

    Add functions to add players in PlayerList

--- src/logic/PlayerList.ts
+++ src/logic/PlayerList.ts
@@ -37,6 +37,34 @@ class PlayerList {
         return this.players[indx];
     }
 
+    getNewPlayer(): Player {
+        var id = -1;
+        while (id === -1 || this.isIDAssigned(id)) {
+            id = Math.floor(Math.random() * 99999999);
+        }
+        return new Player(id, "", false, false, 0);
+    }
+
+    isIDAssigned(id: number) {
+        for (const pl of this.players) {
+            if (pl.id == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    getIndexFromID(id: number): number {
+        var indx = 0;
+        for (const pl of this.players) {
+            if (pl.id == id) {
+                return indx;
+            }
+            indx++;
+        }
+        throw new Error("Player ${id} not found");
+    }
+
     /**
      * Gets the player correstponding to the given ID
      * 
@@ -75,7 +103,17 @@ class PlayerList {
     getActivePlayers(): Player[] {
         var players: Player[] = [];
         for (const pl of this.players) {
-            if (pl.playingState === PlayingState.active) {
+            if (pl.playingState === PlayingState.PLAYING) {
+                players.push(pl)
+            }
+        }
+        return players;
+    }
+
+    getPlayersWithState(state: PlayingState): Player[] {
+        var players: Player[] = [];
+        for (const pl of this.players) {
+            if (pl.playingState === state) {
                 players.push(pl)
             }
         }
@@ -98,7 +136,7 @@ class PlayerList {
      */
     setRoundPlayerInfo(byeID?: number) { 
         for (const pl of this.players) {
-            if (pl.playingState === PlayingState.notplaying || pl.playingState === PlayingState.inactive) {
+            if (pl.playingState === PlayingState.AWAY || pl.playingState === PlayingState.NOTPLAYING) {
                 pl.inrounds.push(false);
                 pl.byes.push(null);
             } else {
@@ -114,6 +152,21 @@ class PlayerList {
                 }
             }
         }
+    }
+
+    getJSON(finaliseranks?: boolean) {
+        if (finaliseranks) {
+            for (var pl of this.players) {
+                pl.startingelo = pl.currentelo
+            }
+        }
+        const replacer = (key: string, value: any) => {
+            if (["currentelo", "inrounds", "byes", "playingState"].includes(key)) {
+                return undefined;
+            }
+            return value
+        }
+        return JSON.stringify({players: this.players}, replacer)
     }
 }
 
